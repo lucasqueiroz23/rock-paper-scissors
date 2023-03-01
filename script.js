04811b3bf846b52930790227cdc1cec55fa42e69
Lucas <lucas.fn4@gmail.com>
2023-03-01 01:51:26 -0300

    Add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('Hello, world!');
+const choices = {
+  0: 'Rock',
+  1: 'Paper',
+  2: 'Scissors',
+};
+
+function getComputerChoice(){
+  const randomNumber = parseInt(Math.random() * 100);
+  return choices[randomNumber%3];
+}
